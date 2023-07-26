bd7ed28a964fb2d117342ab377c3f45453b26315
Emidio Valeretto <emidio.valereto@gmail.com>
2023-07-26 00:18:07 +0000

    chore: add routes to the universe and explore pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ import { Router } from "./routes.js"
 const router = new Router()
 
 router.getRoute('/', "/pages/home.html")
+router.getRoute('/the-universe', "/pages/universe.html")
+router.getRoute('/explore', "/pages/explore.html")
 router.getRoute(404, "/pages/404.html")
 
 router.handle()
