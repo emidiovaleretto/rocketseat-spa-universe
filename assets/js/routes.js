5751913ad9a0592cdf948a4e51f879d18ecacf20
Emidio Valeretto <emidio.valereto@gmail.com>
2023-07-26 18:16:59 +0000

    style: add different background images for each page

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -18,6 +18,16 @@ export class Router {
         const { pathname } = window.location
         const route = this.routes[pathname] || this.routes[404]
 
+        document.querySelector("body").style.transition = "all 2s";
+
+        if (route.includes("home")) {
+            document.querySelector("body").style.backgroundImage = "url('./assets/img/home-bg.png')";
+        } else if (route.includes("universe")) {
+            document.querySelector("body").style.backgroundImage = "url('./assets/img/universe-bg.png')";
+        } else {
+            document.querySelector("body").style.backgroundImage = "url('./assets/img/explore-bg.png')";
+        }
+
         fetch(route)
         .then(data => data.text())
         .then(html => {
